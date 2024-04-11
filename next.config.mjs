6508b5de5325f0ef6dc22b53a148f5f49eb63382
fuegovic <fueg@live.ca>
2024-04-11 16:38:42 -0400

    update redirects

--- next.config.mjs
+++ next.config.mjs
@@ -27,6 +27,22 @@ const cspHeader = `
   block-all-mixed-content;
 `;
 
+const nonPermanentRedirects = [
+  // Up to date Redirects:
+  ["/discord", "https://discord.librechat.ai"],
+  ["/demo", "https://demo.librechat.cfd"],
+  ["/issue", "https://github.com/danny-avila/LibreChat/issues/new/choose"],
+  ["/new-issue", "https://github.com/danny-avila/LibreChat/issues/new/choose"],
+  ["/issues", "https://github.com/danny-avila/LibreChat/issues"],
+  ["/gh-support", "https://github.com/danny-avila/LibreChat/discussions/categories/support"],
+  ["/gh-discussions", "https://github.com/danny-avila/LibreChat/discussions"],
+  ["/roadmap", "/docs/roadmap"],
+  // Redirect to overview pages
+  ...[].map((path) => [path, path + "/overview"]),
+];
+
+const permanentRedirects = []
+
 // nextra config
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
@@ -106,8 +122,5 @@ const nextraConfig = withNextra({
     })),
   ]
 });
-
-const permanentRedirects = []
-
 
 export default withBundleAnalyzer(nextraConfig);
