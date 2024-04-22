c0a05060d4208856254de9f5c8e2f71397cb1e57
Fuegovic <32828263+fuegovic@users.noreply.github.com>
2024-04-22 04:57:46 -0400

    🖥️docs: development (#45)
    * docs: local install guides
    
    * docs: remove old docs
    
    * docs: update breaking changes
    
    * docs: development
    
    * docs: development
    
    * docs: development

---
+++ pages/docs/development/_meta.ts
@@ -0,0 +1,18 @@
+export default {
+  index: 'Intro',
+  get_started: 'Get Started',
+  conventions: 'Coding Conventions',
+  guidelines: {
+    type: 'page',
+    title: 'Contributor Guidelines',
+    href: 'https://github.com/danny-avila/LibreChat/blob/main/.github/CONTRIBUTING.md',
+    newWindow: true,
+  },
+  testing: 'Testing',
+  security: {
+    type: 'page',
+    title: 'Security Policy',
+    href: 'https://github.com/danny-avila/LibreChat/blob/main/.github/SECURITY.md',
+    newWindow: true,
+  },
+}
