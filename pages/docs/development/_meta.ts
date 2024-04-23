9481d584a005653ff166b763b70c0b9791f31848
fuegovic <fueg@live.ca>
2024-04-22 23:45:01 -0400

    docs: development(tools)

--- pages/docs/development/_meta.ts
+++ pages/docs/development/_meta.ts
@@ -1,17 +1,22 @@
 export default {
   index: 'Intro',
   get_started: 'Get Started',
-  translation: 'Contribute a Translation',
+  translation: 'Translation',
+  tools_and_plugins: 'Tools and Plugins',
+  testing: 'Testing',
+  debugging: 'Debugging (WIP)',
+  technical: {
+    // "title": "Contributing",
+    type: 'separator',
+  },
   architecture: 'Project Architecture (WIP)',
-  conventions: 'Coding Conventions',
   guidelines: {
     type: 'page',
     title: 'Contributor Guidelines',
     href: 'https://github.com/danny-avila/LibreChat/blob/main/.github/CONTRIBUTING.md',
     newWindow: true,
   },
-  testing: 'Testing',
-  debugging: 'Debugging (WIP)',
+  conventions: 'Coding Conventions',
   security: {
     type: 'page',
     title: 'Security Policy',
