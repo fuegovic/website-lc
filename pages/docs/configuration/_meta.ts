9bb2773ca8871994827624e895c23de5bebc2c25
fuegovic <fueg@live.ca>
2024-05-02 20:26:55 -0400

    docs: librechat.yaml (wip)

--- pages/docs/configuration/_meta.ts
+++ pages/docs/configuration/_meta.ts
@@ -2,6 +2,7 @@ export default {
   index: 'Intro',
   authentication: 'Authentication',
   docker: 'Docker',
+  librechat_yaml: 'librechat.yaml',
   mongodb: 'MongoDB',
   pre_configured_ai: 'Pre-Configured AIs',
   tools: 'Tools and Plugins',
