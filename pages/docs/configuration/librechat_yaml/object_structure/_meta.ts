70e0976c0426ae269897b64e56cfbab5eaa01f6d
fuegovic <fueg@live.ca>
2024-05-04 16:42:06 -0400

    docs: librechat.yaml screenshots

---
+++ pages/docs/configuration/librechat_yaml/object_structure/_meta.ts
@@ -0,0 +1,10 @@
+export default {
+  config: 'Config',
+  file_config: 'File Config',
+  interface: 'Interface',
+  model_specs: 'Model Specs',
+  registration: 'Registration',
+  assistants_endpoint: 'Assistants Endpoint',
+  custom_endpoint: 'Custom Endpoint',
+  azure_openai: 'Azure OpenAI',
+}
