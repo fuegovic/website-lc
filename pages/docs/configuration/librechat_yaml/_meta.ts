c27a70dd72cd35d7b8c01f933fc4d21c25cf2dd7
fuegovic <fueg@live.ca>
2024-05-04 14:45:18 -0400

    docs: librechat.yaml

--- pages/docs/configuration/librechat_yaml/_meta.ts
+++ pages/docs/configuration/librechat_yaml/_meta.ts
@@ -3,6 +3,5 @@ export default {
   ai_endpoints: 'Custom AI Endpoints',
   setup: 'Setup',
   example: 'Example Configs',
-  config_structure: 'Config Structure',
-  file_config_structure: 'File Config Object Structure',
+  object_structure: 'Object Structure',
 }
